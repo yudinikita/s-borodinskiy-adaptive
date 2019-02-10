c5688ea4f3da03b86926187963690a9cf90568f5
BlackNinja <nikitayudin782@gmail.com>
2019-02-10 23:49:54 +0300

    добавил скрипт для открытия/закрытия меню на мобильном

---
+++ source/js/toggle.js
@@ -0,0 +1,16 @@
+toggleFunction();
+
+function toggleFunction() {
+  const navMain = document.querySelector('.main-nav');
+  const navToggle = navMain.querySelector('.main-nav__toggle');
+  navMain.classList.remove('main-nav--nojs');
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+}
